ace0220590313ef256db2b8ace6fd5e313aa8a32
ozzievision <jasonsargentpetportraits@gmail.com>
2025-05-05 08:44:40 +0100

    Update formhandler.php

--- formhandler.php
+++ formhandler.php
@@ -1,16 +1,16 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-   $homeCountry = htmlspecialchars($_POST["homeCountry"]);
-   $points12 = htmlspecialchars($_POST["points12"]);
-   $points10 = htmlspecialchars($_POST["points10"]);
-   $points8 = htmlspecialchars($_POST["points8"]);
-   $points7 = htmlspecialchars($_POST["points7"]);
-   $points6 = htmlspecialchars($_POST["points6"]);
-   $points5 = htmlspecialchars($_POST["points5"]);
-   $points4 = htmlspecialchars($_POST["points4"]);
-   $points3 = htmlspecialchars($_POST["points3"]);
-   $points2 = htmlspecialchars($_POST["points2"]);
-   $points1 = htmlspecialchars($_POST["points1"]);
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+   $homeCountry = htmlspecialchars($_GET["homeCountry"]);
+   $points12 = htmlspecialchars($_GET["points12"]);
+   $points10 = htmlspecialchars($_GET["points10"]);
+   $points8 = htmlspecialchars($_GET["points8"]);
+   $points7 = htmlspecialchars($_GET["points7"]);
+   $points6 = htmlspecialchars($_GET["points6"]);
+   $points5 = htmlspecialchars($_GET["points5"]);
+   $points4 = htmlspecialchars($_GET["points4"]);
+   $points3 = htmlspecialchars($_GET["points3"]);
+   $points2 = htmlspecialchars($_GET["points2"]);
+   $points1 = htmlspecialchars($_GET["points1"]);
    echo "for the country";
    echo "<br>";
    echo $homeCountry;
